e791481802abc61eb2e544c0d892a126eff77112
Miguel Enrique Saca Accostupa <miguelenrique1805@gmail.com>
2022-07-05 21:11:36 -0500

    add ejecution in php

---
+++ EjecucionArchivo.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+
+<?php
+          //Primer Programa escrito en PHP 
+    echo"<b>Bienvenidos</b><br><b>Estudiantes UNSAAC</b>"; 
+?>
+
+<?php 
+    //Primer Programa escrito en PHP
+    echo "Bienvenidos\n Hola Estudiantes UNSAAC \n"; 
+?>
+
+
